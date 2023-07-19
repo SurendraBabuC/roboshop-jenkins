4b919000a8d8636393cd728a09521b314492c59d
C SURENDRA BABU <surendrababu.c01@gmail.com>
2023-07-19 23:17:12 +0530

    Ansible

---
+++ vars/nodejs.groovy
@@ -0,0 +1,52 @@
+def call() {
+    pipeline {
+
+        agent {
+            node {
+                label 'workstation'
+            }
+        }
+
+        options {
+            ansiColor('xterm')
+        }
+
+        stages {
+
+            stage('Code Quality') {
+                steps {
+                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.88.90:9000 -Dsonar.login=admin -Dsonar.password=admin123'
+                }
+            }
+
+            stage('Unit Test Cases') {
+                steps {
+                    sh 'echo Unit Test Cases'
+                }
+            }
+
+            stage('CheckMarx SAST Scan') {
+                steps {
+                    sh 'echo CheckMarx SAST Scan'
+                }
+            }
+
+            stage('CheckMarx SCA Scan') {
+                steps {
+                    sh 'echo CheckMarx SCA Scan'
+                }
+            }
+
+        }
+
+
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
+    }
+
+}
