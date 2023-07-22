b558ffd6ec4c091b79ef46f919d05c1b417528a4
C SURENDRA BABU <surendrababu.c01@gmail.com>
2023-07-22 20:12:21 +0530

    Ansible

---
+++ vars/nginx.groovy
@@ -0,0 +1,70 @@
+def call() {
+    pipeline {
+
+        agent {
+            node {
+                label 'workstation'
+            }
+        }
+
+        options {
+            ansiColor('xterm')
+        }
+
+        stages {
+
+            stage('Code Quality') {
+                steps {
+//                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.88.90:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
+                    sh 'echo Code Quality'
+                }
+            }
+
+            stage('Unit Test Cases') {
+                steps {
+                    sh 'echo Unit Test Cases'
+//                    sh 'npm test'
+                }
+            }
+
+            stage('CheckMarx SAST Scan') {
+                steps {
+                    sh 'echo CheckMarx SAST Scan'
+                }
+            }
+
+            stage('CheckMarx SCA Scan') {
+                steps {
+                    sh 'echo CheckMarx SCA Scan'
+                }
+            }
+
+            stage('Release Application') {
+                when {
+                    expression {
+                        env.TAG_NAME ==~ ".*"
+                    }
+                }
+                steps {
+                    sh 'npm install'
+                    sh 'echo $TAG_NAME >VERSION'
+                    sh 'zip -r ${component}-${TAG_NAME}.zip *'
+                    // Deleting this file as it is not needed
+                    sh 'zip -d ${component}-${TAG_NAME}.zip Jenkinsfile'
+                    sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.5.236:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+                }
+            }
+
+        }
+
+
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
+    }
+
+}
